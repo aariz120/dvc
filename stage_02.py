d69809b8fa2418117a2ee05c1da32c99b7797ac1
aariz120 <aariz120@gmail.com>
2023-07-03 19:41:16 +0530

    stage 3 added and made changes in yaml

--- stage_02.py
+++ stage_02.py
@@ -1,4 +1,6 @@
 with open("artifact.txt","r") as f:
     text=f.read()
 
-print(text)    
+print(text)
+print("it is  end of stage 2")  
+print("i am going to add stage 3 ")
---
+++ stage_03.py
@@ -0,0 +1,11 @@
+with open("artifact.txt","r") as f:
+    text=f.read()
+
+with open("artifact.txt","w") as f:
+    text=f.write(text+"i have added one file")  
+
+
+
+print(text)
+print("it is a end of stage 3")
+  
